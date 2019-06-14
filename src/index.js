97f75a70bf4fb74a4783b4ec47d3eeed23c59ea9
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2019-06-14 08:07:16 -0300

    Adding route to access the resized images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 mongoose.connect('acessar o mongo atlas e criar cluster, adicionar um novo usuario e liberar acesso na rede', {
     useNewUrlParser: true,
 });
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
