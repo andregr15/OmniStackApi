afa64ae26a931ab6c8bd7113f9ddc03668aac231
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2019-06-14 08:05:52 -0300

    Adding logic to resize the uploaded image

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -4,9 +4,10 @@ module.exports = {
     async store(req, res) {
         const post = await Post.findById(req.params.id);
 
-        post.like += 1;
+        post.likes += 1;
 
         await post.save();
+        
         return res.json(post);
     }
 };
