3b6daf85e6c85595c19b1f13330b2c16b30989a0
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2019-06-14 08:09:36 -0300

    Adding cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect('acessar o mongo atlas e criar cluster, adicionar um novo usuario e liberar acesso na rede', {
     useNewUrlParser: true,
 });
+
+app.use(cors());
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
